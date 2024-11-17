e3537c45c9cbc73620eb2c8ed2efd55eb064331c
rexcy305 <672023236@student.uksw.edu>
2024-11-18 00:39:34 +0700

    test commit by rexcy

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coffey</title>
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="/fontawesome/css/font-awesome.min.css">
+    <script src="/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-dark sticky-top">
+        <div class="container-fluid w-75 mt-3">
+            <a class="navbar-brand fs-6 fw-bold Brand" href="#">
+                <i class="fa fa-coffee fa-2x" aria-hidden="true"></i>
+                CoffeinAja
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse mx-5 px-3" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 fw-bold">
+                    <li class="nav-item">
+                        <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item px-5">
+                        <a class="nav-link text-light" href="#">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Location
+                        </a>
+                        <!-- <ul class="dropdown-menu text-light">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul> -->
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <button class="btn bg-transparent">
+                        <i class="fa fa-search cari" aria-hidden="true"></i>
+                    </button>
+                </form>
+                <div class="ms-5">
+                    <a href="" class="text-decoration-none text-light d-flex flex-column align-items-center justify-content-center">
+                        <button class="profile">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container text-light mt-3">
+        <div class="row">
+            <div class="col-7 mt-5 pt-5 frontlook">
+                <h2 class="mx-5 px-5">Salatiga</h2>
+                <h1 class="mx-5 px-5">Coffe Nomerouno</h1>
+                <p class="mx-5 px-5 pt-2">Coffe no kafein ril no vake cuy. Harus Beli, Wajib!</p>
+                <div class="mx-5 px-1">
+                    <button class="btn fw-bold rounded-pill mx-5 b-beli">Rp 5.000.000</button>
+                </div>
+            </div>
+            <div class="col-5 img-fluid mt-3 pt-5">
+                <img class="g-kopi" src="/Source/1.png" alt="Gambar Kopi">
+            </div>
+        </div>
+    </div>
+    <div class="container mb-5 front">
+        <div class="front-contain">
+            <div class="d-flex flex-row justify-content-evenly align-items-center pt-5 frontmenu">
+                <div class="bg-light d-flex flex-column align-items-center justify-content-center p-4 shadow-sm">
+                    <img src="/Source/5.png" alt="Kopi-4" class="img-fluid">
+                    <h6 class="mt-3 text-center">Kopi-4</h6>
+                    <h5 class="text-center">Rp 5M</h5>
+                    <div class="d-flex align-items-center justify-content-center mt-2 w-75 tambah-barang">
+                        <button class="btn-icon">
+                            <i class="fa fa-minus-circle fs-4 me-3 cursor-pointer" aria-hidden="true"></i>
+                        </button>
+                        <p class="mb-0">0</p>
+                        <button class="btn-icon">
+                            <i class="fa fa-plus-circle text-primary fs-4 ms-3 cursor-pointer" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="bg-light d-flex flex-column align-items-center justify-content-center p-4 shadow-sm">
+                    <img src="/Source/3.png" alt="">
+                    <h6 class="mt-3">Kopi-2</h6>
+                    <h5>Rp 5M</h5>
+                    <div class="d-flex align-items-center justify-content-center mt-2 w-75 tambah-barang">
+                        <button class="btn-icon">
+                            <i class="fa fa-minus-circle fs-4 me-3 cursor-pointer" aria-hidden="true"></i>
+                        </button>
+                        <p class="mb-0">0</p>
+                        <button class="btn-icon">
+                            <i class="fa fa-plus-circle text-primary fs-4 ms-3 cursor-pointer" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="bg-light d-flex flex-column align-items-center justify-content-center p-4 shadow-sm">
+                    <img src="/Source/5.png" alt="">
+                    <h6 class="mt-3">Kopi-3</h6>
+                    <h5>Rp 5M</h5>
+                    <div class="d-flex align-items-center justify-content-center mt-2 w-75 tambah-barang">
+                        <button class="btn-icon">
+                            <i class="fa fa-minus-circle fs-4 me-3 cursor-pointer" aria-hidden="true"></i>
+                        </button>
+                        <p class="mb-0">0</p>
+                        <button class="btn-icon">
+                            <i class="fa fa-plus-circle text-primary fs-4 ms-3 cursor-pointer" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="bg-light d-flex flex-column align-items-center justify-content-center p-4 shadow-sm">
+                    <img src="/Source/5.png" alt="">
+                    <h6 class="mt-3">Kopi-4</h6>
+                    <h5>Rp 5M</h5>
+                    <div class="d-flex align-items-center justify-content-center mt-2 w-75 tambah-barang">
+                        <button class="btn-icon">
+                            <i class="fa fa-minus-circle fs-4 me-3 cursor-pointer" aria-hidden="true"></i>
+                        </button>
+                        <p class="mb-0">0</p>
+                        <button class="btn-icon">
+                            <i class="fa fa-plus-circle text-primary fs-4 ms-3 cursor-pointer" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="bg-light d-flex flex-column align-items-center justify-content-center p-4 shadow-sm">
+                    <img src="/Source/5.png" alt="">
+                    <h6 class="mt-3">Kopi-5</h6>
+                    <h5>Rp 5M</h5>
+                    <div class="d-flex align-items-center justify-content-center mt-2 w-75 tambah-barang">
+                        <button class="btn-icon">
+                            <i class="fa fa-minus-circle fs-4 me-3 cursor-pointer" aria-hidden="true"></i>
+                        </button>
+                        <p class="mb-0">0</p>
+                        <button class="btn-icon">
+                            <i class="fa fa-plus-circle text-primary fs-4 ms-3 cursor-pointer" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="bg-light d-flex flex-column align-items-center justify-content-center p-4 shadow-sm">
+                    <img src="/Source/5.png" alt="">
+                    <h6 class="mt-3">Kopi-6</h6>
+                    <h5>Rp 5M</h5>
+                    <div class="d-flex align-items-center justify-content-center mt-2 w-75 tambah-barang">
+                        <button class="btn-icon">
+                            <i class="fa fa-minus-circle fs-4 me-3 cursor-pointer" aria-hidden="true"></i>
+                        </button>
+                        <p class="mb-0">0</p>
+                        <button class="btn-icon">
+                            <i class="fa fa-plus-circle text-primary fs-4 ms-3 cursor-pointer" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
