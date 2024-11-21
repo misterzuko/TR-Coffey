483812a200586f2e3b666fbd5de3e4025f32f13b
ANR091 <anr00091@gmail.com>
2024-11-22 01:08:28 +0700

    ini admin hehehehh

---
+++ main_code/admin.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coffey-ADMIN</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="../fontawesome/css/font-awesome.min.css">
+    <script src="../js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
