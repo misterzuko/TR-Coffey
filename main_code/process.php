352c6d306c4313a0b2b262173fcb4626e1cca253
ANR091 <anr00091@gmail.com>
2024-11-21 23:42:12 +0700

    udah tak tambahi buat masuk ke admin.php & regis

--- main_code/process.php
+++ main_code/process.php
@@ -8,19 +8,32 @@
             $sql = mysqli_query($conn,$query);
             $result = mysqli_fetch_assoc($sql);
             if($result!=null){
-                echo "Anda login sebagai ".$result['username'];
+                if($result['username']=="ADMIN" and $result['password']=="ADMIN#1234"){
+                    header('location: admin.php');
+                } else {
                 $query = "SELECT email,username FROM tb_akun WHERE email='$email' AND password='$password'";
                 $sql = mysqli_query($conn,$query);
                 session_start();
                 $result = mysqli_fetch_assoc($sql);
                 $_SESSION['credential']=$result;
                 header('location: user.php');
+                }
             } else {
                 header('location: login.php?errorlog');
             }
             // header('user.php');
             }
             catch(Exception $e){
+                echo "error".$e;
+            }
+        } elseif (isset($_POST['register'])) {
+            try{
+            $email = $_POST['email'];
+            $username = $_POST['username'];
+            $password = $_POST['pw'];
+            $query = "INSERT INTO tb_akun(email,username,password) VALUES ('$email','$username','$password')";
+            echo "Register berhasil bjir";
+            } catch(Exception $e){
                 echo "error".$e;
             }
         }
