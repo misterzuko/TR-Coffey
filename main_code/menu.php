84bd29eab598f094ba83fa5e6084c5a72f7cfe3b
rexcy305 <672023236@student.uksw.edu>
2024-12-04 01:29:55 +0700

    begitu

--- main_code/menu.php
+++ main_code/menu.php
@@ -117,39 +117,39 @@
             </div>
         </div>
     </nav>
-    <div class="container d-flex justify-content-center align-items-center menu-body mt-1">
+    <div class="d-flex justify-content-center align-items-center menu-body mt-1">
         <form class="row g-3 needs-validation" method="POST" action="process.php" novalidate>
             <div class="col-md-4 d-flex flex-column align-items-center justify-content-center card-menu">
                 <img src="../src/<?php echo $kopi;?>.png" alt="Kopi-4" class="img-fluid img-menu">
             </div>
             <div class="col-md-8 ms-1 row g-3">
-                <h5 class="col-md-12 mt-3 fw-medium"><?php echo $result['nama_barang'];?></h5>
-                <p class="col-md-12 fw-medium fs-3 harga">Rp <text id="harga-kopi"><?php echo $result['harga_barang'];?></text></p>
-                <div class="col-md-12 row g-3">
+                <h5 class="col-md-12 mt-3 fs-5 fw-medium"><?php echo $result['nama_barang'];?></h5>
+                <p class="col-md-12 fw-bold fs-2 harga">Rp <text id="harga-kopi"><?php echo $result['harga_barang'];?></text></p>
+                <div class="col-md-12 row g-3 komponen">
                     <div class="col-md-5 row g-3 d-flex align-items-center justify-content-center">
-                        <div class="col-md-12 form-check fw-medium text-center">
+                        <div class="col-md-12 form-check fw-bold text-center">
                             <p>Jenis Penyajian</p>
                         </div>
                         <div class="col-md-12 d-flex align-items-center justify-content-evenly text-center">
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" name="saji" id="dinginpanas" value="Panas" onchange="hargaUpdater()" checked>
                                 <label class="form-check-label" for="panas">
-                                    <p class="fw-medium">Hot</p>
+                                    <p class="fw-bold">Hot</p>
                                 </label>
                             </div>
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" name="saji" id="dinginpanas" value="Dingin" onchange="hargaUpdater()">
                                 <label class="form-check-label" for="dingin">
-                                    <p class="fw-medium">Cold</p>
+                                    <p class="fw-bold">Cold</p>
                                 </label>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-7 row g-3 d-flex align-items-center justify-content-start fs-6">
-                        <div class="col-md-12 fw-medium text-center">
+                    <div class="col-md-7 row g-3 d-flex align-items-center justify-content-evenly">
+                        <div class="col-md-12 fw-bold text-center komponen">
                             <p>Ukuran Cup</p>
                         </div>
-                        <div class="col-md-12 d-flex align-items-center justify-content-evenly">
+                        <div class="col-md-12 d-flex align-items-center justify-content-evenly komponen">
                             <?php
                             for($i=1;isset($_SESSION['data-cup'][$i]['id_barang']);$i++){
                                 if($_SESSION['data-cup'][$i]['stok_barang']>0){
@@ -157,7 +157,7 @@
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" name="ukuran" value="<?php echo $i;?>" id="ukuran" onchange="updatevalcup(<?php echo $i;?>)" <?php if($i==1)echo 'checked';?>>
                                 <label class="form-check-label" for="<?php echo $i;?>">
-                                    <p class="fw-medium"><?php echo $_SESSION['data-cup'][$i]['nama_barang'];?></p>
+                                    <p class="fw-bold"><?php echo $_SESSION['data-cup'][$i]['nama_barang'];?></p>
                                 </label>
                             </div>
                             <?php
@@ -166,7 +166,7 @@
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" name="ukuran" value="<?php echo $i;?>" id="ukuran" onchange="updatevalcup(<?php echo $i;?>)" disabled>
                                 <label class="form-check-label" for="<?php echo $i;?>" style="color: grey;">
-                                    <p class="fw-medium"><?php echo $_SESSION['data-cup'][$i]['nama_barang'];?></p>
+                                    <p class="fw-bold"><?php echo $_SESSION['data-cup'][$i]['nama_barang'];?></p>
                                 </label>
                             </div>
                             <?php
@@ -176,10 +176,10 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-12 row g-3 d-flex align-items-center justify-content-center">
+                <div class="col-md-12 row g-3 d-flex align-items-center justify-content-center komponen">
                     <div class="col-md-5 d-flex align-items-center justify-content-center">
                         <div class="container text-center">
-                            <label for="topping" class="form-label fw-medium">Pilih Topping</label>
+                            <label for="topping" class="form-label fw-bold">Pilih Topping</label>
                             <select name="topping" class="form-select w-75 mx-auto" id="topping" onchange="updatemax()" required>
                                 <?php
                                 for($i=1;isset($_SESSION['data-topping'][$i]['id_barang']);$i++){
@@ -195,25 +195,25 @@
                     </div>
                     <div class="col-md-7 d-flex align-items-center justify-content-center">
                         <div class="container text-center">
-                            <label for="jmlhtopping" class="form-label fw-medium">Metode Pembayaran</label>
+                            <label for="jmlhtopping" class="form-label fw-bold">Metode Pembayaran</label>
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" value="tunai" name="medpem" id="medpem" checked>
                                 <label class="form-check-label" for="tunai">
-                                    <p class="fw-medium">Tunai</p>
+                                    <p class="fw-bold">Tunai</p>
                                 </label>
                             </div>
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" value="non-tunai"  name="medpem" id="medpem">
                                 <label class="form-check-label" for="nontunai">
-                                    <p class="fw-medium">Non Tunai</p>
+                                    <p class="fw-bold">Non Tunai</p>
                                 </label>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-12 row g-3 d-flex align-items-start justify-content-center">
+                <div class="col-md-12 row g-3 d-flex align-items-start justify-content-center proses">
                     <div class="col-md-8">
-                        <p class="fw-medium fs-5">Total Harga: Rp <input type="text" id="harga" name="total" value="" readonly class="border-0 bg-tohar text-dark" disabled></p>
+                        <p class="fw-bold fs-5">Total Harga: Rp <input type="text" id="harga" name="total" value="" readonly class="border-0 bg-tohar text-dark fw-bold" disabled></p>
                     </div>
                     <div class="col-md-4 d-flex align-items-center justify-content-end">
                         <button type="submit" class="btn-mine fw-bold" name="proses">Proses</button>
