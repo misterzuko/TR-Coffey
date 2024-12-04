0eb2e2c2ef6bbf91ae520b14e2726b5e75712ed9
dre_45 <sitepuandras6@gmail.com>
2024-12-04 15:13:43 +0700

    styling kasir

--- main_code/recordhistory.php
+++ main_code/recordhistory.php
@@ -31,15 +31,43 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Record Transaksi</title>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/kasir.css">
+    <link rel="stylesheet" href="styles.css?v=1.1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;700&display=swap">
+    <link rel="stylesheet" href="../css/riwayat.css">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../fontawesome/css/font-awesome.min.css">
+    <script src="../js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <div class="container">
-        <h1>Record Transaksi</h1>
-        <table>
+<nav class="navbar navbar-expand-lg navbar-transparent sticky-top">
+        <div class="container-fluid w-75 mt-3">
+            <a class="navbar-brand fw-bold Brand row" href="kasir.php">
+                <i class="fa fa-coffee fa-2x col-3" aria-hidden="true"></i>
+                <p class="col-9 pt-2"><span>kopi</span>inaja</p>
+            </a>
+            <div class="collapse navbar-collapse ms-5 ps-3 ham-nav" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a aria-current="page" href="kasir.php">Home</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a aria-current="page" href="recordhistory.php">Riwayat</a>
+                    </li>
+                </ul>
+            </div>
+        </div>   
+    </nav>
+    <div class="containt">
+        <table class = "table containt-record table-striped-columns">
             <thead>
                 <tr>
+                    <th colspan="11" class = "text-center fs-3 bg-secondary">Riwayat Transaksi</th>
+                </tr>
+                <tr class = "text-center">
                     <th>ID Pesanan</th>
                     <th>Nama Pemesan</th>
                     <th>Jenis Kopi</th>
@@ -57,7 +85,7 @@ $result = $conn->query($sql);
                 <?php
                 if ($result->num_rows > 0) {
                     while ($row = $result->fetch_assoc()) {
-                        echo "<tr>
+                        echo "<tr class = 'text-center'>
                                 <td>{$row['id_pesanan']}</td>
                                 <td>{$row['nama_pemesan']}</td>
                                 <td>{$row['jenis_kopi']}</td>
@@ -82,7 +110,7 @@ $result = $conn->query($sql);
                         
                             echo "<form action='' method='POST'>
                                     <input type='hidden' name='id_pesanan' value='{$row['id_pesanan']}'>
-                                    <a href='cetak_struk.php?id_pesanan={$row['id_pesanan']}' class='btn-cetak'>Cetak Transaksi</a>
+                                    <a href='cetak_struk.php?id_pesanan={$row['id_pesanan']}' class='btn btn-warning fs-small'>Cetak</a>
                                   </form>";
                         }
 
@@ -94,8 +122,18 @@ $result = $conn->query($sql);
                 ?>
             </tbody>
         </table>
-        <a href="kasir.php" class="back">Kembali ke Kasir</a>
     </div>
+    <script> 
+        document.addEventListener("DOMContentLoaded", function () {
+            const navbar = document.querySelector('.navbar');
+            navbar.classList.add('navbar-solid');
+        });
+
+        document.addEventListener("DOMContentLoaded", function () {
+        const navbar = document.querySelector('.navbar');
+        navbar.classList.add('navbar-solid');
+        });
+    </script>
 </body>
 </html>
 
